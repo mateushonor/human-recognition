0b98a5474131775c15cf364399f43939b41de269
mateushonor <mateushonorato@ymail.com>
2024-04-14 14:51:19 -0300

    Codigo só com Yolo

--- Openc_threads.py
+++ Openc_threads.py
@@ -6,14 +6,10 @@ import time
 # Carregar o modelo YOLO
 model = YOLO("yolo-Weights/yolov8n.pt")
 
-# Inicializa o detector HOG
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-
 # Definições das câmeras
 cameras = {
-    "Camera1": "rtsp://Honor:Mateus2449@192.168.100.158:554/onvif1",
-    "Camera2": "rtsp://udne:en4t75@192.168.100.216:554/onvif2"
+    "Camera1": "rtsp://Honor:Mateus2449@192.168.100.158:554/onvif1?overrun_nonfatal=1&fifo_size=50000000",
+    "Camera2": "rtsp://udne:en4t75@192.168.100.216:554/onvif2?overrun_nonfatal=1&fifo_size=50000000"
 }
 
 def process_camera(url, camera_name):
@@ -29,28 +25,27 @@ def process_camera(url, camera_name):
 
             ret, frame = cap.read()
             if not ret:
-                print("Frame não foi recebido corretamente")
-                break
+                print(f"Frame não foi recebido corretamente em {camera_name}")
+                continue  # Em vez de quebrar, continua tentando
 
-            # Detecção preliminar usando HOG
-            (rects, weights) = hog.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.05)
+            # Detecção usando YOLO
+            results = model(frame, stream=True)
 
-            # Processamento com YOLO apenas se HOG detectar uma pessoa
-            if len(rects) > 0:
-                results = model(frame, stream=True)
-
-                for r in results:
-                    boxes = r.boxes
-                    for box in boxes:
-                        if int(box.cls[0]) == 0:  # Verifica se a classe detectada é 'person'
-                            x1, y1, x2, y2 = map(int, box.xyxy[0])
-                            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            # Desenho das caixas delimitadoras baseadas nas detecções de YOLO
+            for r in results:
+                boxes = r.boxes
+                for box in boxes:
+                    x1, y1, x2, y2 = map(int, box.xyxy[0])
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
             cv2.imshow(f'VIDEO {camera_name}', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
-            time.sleep(max(1.0 / 30 - (time.time() - start_time), 0))  # Taxa de 30 FPS
+            # Manter a taxa de 30 FPS
+            frame_time = time.time() - start_time
+            if frame_time < 1.0 / 30:
+                time.sleep(1.0 / 30 - frame_time)
 
     finally:
         cap.release()
