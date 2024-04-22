99f889f43336cf4c08074fd2c415725893464c22
GabrielAlvsc <gabrielalves2002@gmail.com>
2024-04-22 12:47:39 -0300

    sender com twilio, venv(producao)

--- SMS_Keys.py
+++ SMS_Keys.py
@@ -1,5 +1,5 @@
 account_sid = 'AC181d5aa4483d49b433293e0ecd022abf'
-auth_token ='5a662120dc36c87f0a3afe019683e4ff'
+auth_token ='70faadb9a1cb0ccf799daf05000eeb56'
 #recovery ='HXNCH167CJ96MP66V5E6FDPB'
 twilio_number ='+12512701223'
 target_number ='+5534999062849'
--- SMS_Sender.py
+++ SMS_Sender.py
@@ -1,23 +1,35 @@
-# import twilio
-# from twilio.rest import Client
-# import SMS_Keys as keys
+from twilio.rest import Client
+import SMS_Keys as keys
 
 
-# def messageSender():
+def messageSender():
 
-#     client = Client(keys.account_sid,keys.auth_token)
+    client = Client(keys.account_sid,keys.auth_token)
 
-#     message = client.messages.create(
-#         body="TEST MESSAGE",
-#         from_=keys.twilio_number,
-#         to=keys.target_number
-#     )
+    message = client.messages.create(
+        body="TEST MESSAGE",
+        from_=keys.twilio_number,
+        to=keys.target_number
+    )
 
-#     return message
+    return message
 
-# print(messageSender().body)
+print(messageSender().body)
 
-import subprocess
-import sys
+#import subprocess
 
-subprocess.Popen("curl https://api.twilio.com/2010-04-01/Accounts/AC181d5aa4483d49b433293e0ecd022abf/Messages.json -X POST --data-urlencode To=+5534999062849 --data-urlencode From=+12512701223 --data-urlencode Body=incurll -u AC181d5aa4483d49b433293e0ecd022abf:70faadb9a1cb0ccf799daf05000eeb56", stdin=subprocess.PIPE, stdout=subprocess.PIPE,stderr=subprocess.PIPE, shell=True)
+
+# message="TESTE 123"
+# apiurl = "https://api.twilio.com/2010-04-01/Accounts/AC181d5aa4483d49b433293e0ecd022abf/Messages.json"
+# method = "POST"
+# to = f"--data-urlencode To={keys.target_number}"
+# from_ = f"--data-urlencode From={keys.twilio_number}"
+# access = f"-u {keys.account_sid}:{keys.auth_token}"
+# msgbody = f"--data-urlencode Body={message}"
+# sender = f"curl {apiurl} -X {method} {to} {from_} {msgbody} {access}"
+
+
+
+# p = subprocess.Popen(sender, stdin=subprocess.PIPE, stdout=subprocess.PIPE,stderr=subprocess.PIPE, shell=True)
+
+# print(p)
